022025aade18d0faa5f9de96fb1925d2278cb701
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-09 15:05:56 +0900

    [add]front-7 Carsページ
    サンプルサイトの導入

---
+++ src/views/Cars.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-row>
+        <el-col :span="24">
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>仮想通貨一覧</span>
+                </div>
+                <div>...</div>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+  export default {
+    name: "Cars"
+  }
+</script>
+
+<style scoped>
+
+</style>
