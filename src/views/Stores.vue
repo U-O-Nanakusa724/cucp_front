d88f1b6d15b636314e2e0c24da9cb67a5181aa41
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-10 18:02:25 +0900

    [update]front-7 Carページ
    フォーム付きダイアログ表示サンプル

--- src/views/Stores.vue
+++ src/views/Stores.vue
@@ -1,6 +1,48 @@
 <template>
-   <div class="store">Store</div>
+   <div class="store">
+
+   <el-button size="mini"
+              type="primary"
+              icon="el-icon-edit"
+              round @click="dialogFormVisible = true"></el-button>
+
+   <el-dialog title="Shipping address" :visible.sync="dialogFormVisible">
+     <el-form :model="form">
+       <el-form-item label="販売店名" :label-width="formLabelWidth">
+         <el-input v-model="form.name" autocomplete="off"></el-input>
+       </el-form-item>
+       <el-form-item label="プルダウン" :label-width="formLabelWidth">
+         <el-select v-model="form.region" placeholder="Please select a zone">
+           <el-option label="Zone No.1" value="shanghai"></el-option>
+           <el-option label="Zone No.2" value="beijing"></el-option>
+         </el-select>
+       </el-form-item>
+     </el-form>
+     <span slot="footer" class="dialog-footer">
+       <el-button @click="dialogFormVisible = false">Cancel</el-button>
+       <el-button type="primary" @click="dialogFormVisible = false">Confirm</el-button>
+     </span>
+   </el-dialog>
+   </div>
 </template>
 
-<style scoped>
-</style>
+<script>
+  export default {
+    data() {
+      return {
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px'
+      };
+    }
+  };
+</script>
