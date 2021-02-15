dc198b7a6298d4ee2c2f5290fe38156625d74f15
“U-O-Nanakusa724“ <u.o.nanakusa724.engineer@gmail.com>
2021-02-15 15:57:11 +0900

    [update]front-7 Carページ
    全体的なCSS調整と検索フォーム、新規ボタン作成

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -11,16 +11,10 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 #header-nav {
   padding: 30px;
+  text-align: center;
 
   a {
     font-weight: bold;
