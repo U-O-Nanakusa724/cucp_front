b1e3bbf0c7578c527cb39c6ea3affea8aa4a81ca
U-O-Nanakusa724 <u.o.nanakusa724.engineer@gmail.com>
2021-02-21 23:46:29 +0900

    [add]front-12 Graph.vue作成
    chart.jsの依存性追加

--- src/components/graph/LineChart.vue
+++ src/components/graph/LineChart.vue
@@ -1,65 +1,76 @@
 <script>
-import { Line } from 'vue-chartjs';
+import { Line } from "vue-chartjs";
 
 export default {
   extends: Line,
-  name: 'chart',
-  data () {
+  name: "chart",
+  data() {
     return {
       data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        labels: ["January", "February", "March", "April", "May", "June"],
         datasets: [
           {
-            label: 'Bar Dataset',
-            data: [10, 20, 30, 40, 50, 30],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+            label: "Line Dataset",
+            data: [
+              {
+                x: "January",
+                y: 0,
+              },
+              {
+                x: "March",
+                y: 10,
+              },
+              {
+                x: "May",
+                y: 5,
+              },
             ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          },
-          {
-            label: 'Line Dataset',
-            data: [10, 50, 20, 30, 30, 40],
-            borderColor: '#CFD8DC',
+            borderColor: "#CFD8DC",
             fill: false,
-            type: 'line',
             lineTension: 0.3,
-          }
-        ]
+            borderWidth: 3,
+            spanGaps: true,
+          },
+        ],
       },
       options: {
+        cutoutPercentage: 0,
+        responsive: true,
+        title: {
+          display: true,
+          text: "価格遷移",
+        },
         scales: {
-          xAxes: [{
-            scaleLabel: {
-              display: true,
-              labelString: 'Month'
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              stepSize: 10,
-            }
-          }]
-        }
-      }
-    }
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "日付",
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                userCallback: function (tick) {
+                  return tick.toString() + "万円";
+                },
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "価格",
+              },
+            },
+          ],
+        },
+      },
+    };
   },
-  mounted () {
-    this.renderChart(this.data, this.options)
-  }
-}
+  methods: {
+    getData: async function () {},
+  },
+  mounted() {
+    this.renderChart(this.data, this.options);
+  },
+};
 </script>
